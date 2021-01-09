a6fb0e90b35c5d9aec2e572b2a07a5cc9787bdf2
Alexandru Balan <61700567+Al3xBM@users.noreply.github.com>
2021-01-09 11:12:39 +0200

    setup related

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flatland.evaluators.client import FlatlandRemoteClient
 from flatland.envs.observations import GlobalObsForRailEnv, ObservationBuilder
 import numpy as np
 import time
-
 #####################################################################
 # Instantiate a Remote Client
 #####################################################################
